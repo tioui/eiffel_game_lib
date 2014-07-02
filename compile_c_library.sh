7f4b6e2325e2a3f8e694774730a06e68a1510fdd
Louis M <prog@tioui.com>
2014-07-02 09:51:40 -0400

    Bug in the compile C library script for Unix

--- compile_c_library.sh
+++ compile_c_library.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-cd audio_snd_files_lib/Clib
-finish_freezing -library
-cd audio_video_lib/Clib
-finish_freezing -library
 cd cpf_lib/Clib
 finish_freezing -library
-cd game_core_lib/Clib
+cd ../../game_core_lib/Clib
+finish_freezing -library
+cd ../../audio_snd_files_lib/Clib
+finish_freezing -library
+cd ../../audio_video_lib/Clib
 finish_freezing -library
